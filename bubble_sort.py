98b98e9be299e3883f3ba90037f8389698ee3888
ThanhTung <thanhtungtb.ptit@gmail.com>
2026-09-23 13:01:50 +0700

    done

--- bubble_sort.py
+++ bubble_sort.py
@@ -2,7 +2,7 @@ n = int(input())
 a = list(map(int,input().split()))
 def bubble_sort(a):
     n = len(a)
-    for end in range(n,0,-1):
+    for end in range(n-1,0,-1):
         for i in range(end):
             if a[i] > a[i+1]:
                 a[i], a[i+1] = a[i+1], a[i]
